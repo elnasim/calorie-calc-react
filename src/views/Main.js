041374030e8617c57370076d706f98b9d8d03964
KTyH <archlis21@gmail.com>
2020-05-18 17:45:01 +0300

    fix loading page

--- src/views/Main.js
+++ src/views/Main.js
@@ -11,10 +11,12 @@ export default function Main() {
     carbohydrates: 0,
     fats: 0,
   });
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const data = read("ration");
     setRation(data);
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -57,28 +59,25 @@ export default function Main() {
     }
   }
 
-  const products =
-    ration &&
-    ration.map((item) => {
-      return (
-        <Product
-          title={item.title}
-          proteins={item.proteins}
-          fats={item.fats}
-          carbohydrates={item.carbohydrates}
-          calories={item.calories}
-          id={item.id}
-          weight={item.weight}
-          remove={removeHandler}
-          key={item.id}
-        />
-      );
-    });
+  const products = ration.map((item) => {
+    return (
+      <Product
+        title={item.title}
+        proteins={item.proteins}
+        fats={item.fats}
+        carbohydrates={item.carbohydrates}
+        calories={item.calories}
+        id={item.id}
+        weight={item.weight}
+        remove={removeHandler}
+        key={item.id}
+      />
+    );
+  });
 
   return (
     <div className="main">
       <div className="container">
-
         {/* <div className="day">
           <div className="day__change">
             <span className="material-icons">undo</span>
@@ -89,34 +88,38 @@ export default function Main() {
           </div>
         </div> */}
 
-        {products}
+        {!isLoading && products}
 
-        <div className="product-main">
-          <div className="product-main__title">Всего</div>
-          <div className="product-main__info">
-            <div className="product-main__i">
-              {summary.proteins}
-              <br />Б
-            </div>
-            <div className="product-main__i">
-              {summary.fats}
-              <br />Ж
-            </div>
-            <div className="product-main__i">
-              {summary.carbohydrates}
-              <br />У
-            </div>
-            <div className="product-main__i">
-              {summary.calories}
-              <br />
-              Ккал
+        {!isLoading && (
+          <div className="product-main">
+            <div className="product-main__title">Всего</div>
+            <div className="product-main__info">
+              <div className="product-main__i">
+                {summary.proteins}
+                <br />Б
+              </div>
+              <div className="product-main__i">
+                {summary.fats}
+                <br />Ж
+              </div>
+              <div className="product-main__i">
+                {summary.carbohydrates}
+                <br />У
+              </div>
+              <div className="product-main__i">
+                {summary.calories}
+                <br />
+                Ккал
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
-        <Link to="/add-ration" className="button">
-          Добавить
-        </Link>
+        {!isLoading && (
+          <Link to="/add-ration" className="button">
+            Добавить
+          </Link>
+        )}
       </div>
     </div>
   );
